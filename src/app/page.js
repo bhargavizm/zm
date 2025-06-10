2260d4f5843f85b6f560df81d6818c80414805c4
pavankrishan <krishnapavan087@gmail.com>
2025-06-10 17:49:06 +0530

    Add reselling page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import MultiURLForm from "@/components/servicesPages/servicesLayout/contentTabs/MultiUrl";
 import GenerateQR from "./generateQR/page";
 
 export default function Home() {
   return (
     <>
       <div className="">
-        < MultiURLForm/>
+        <GenerateQR />
       </div>
     </>
   );
