23caad4463c643baebfa5594492d44f3db3f8e48
navya2012 <navyapadala1995@gmail.com>
2025-06-30 17:18:54 +0530

    add feature images, customization

---
+++ src/components/hooks/usePremiumContext.jsx
@@ -0,0 +1,9 @@
+
+
+import { PremiumContext } from "@/context/premiumContext/PremiumContext";
+import { useContext } from "react";
+
+
+const usePremiumContext = () => useContext(PremiumContext);
+
+export default usePremiumContext;
