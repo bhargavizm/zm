6bb79948363f2e47411ecb553192b8745d60aac1
JosephKorivi <joseph.korivi0911@gmail.com>
2025-07-07 15:20:27 +0530

    add feature robots.txt

--- src/redux/slices/servicesSlice.js
+++ src/redux/slices/servicesSlice.js
@@ -1,198 +1,109 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  // Regular (secured) services
-  resumeServiceData: [],
-  wifiServicesData: [],
-  menuCardServiceData: [],
-  textMessageData: [],
-  smsServiceData: [],
-  vehicleServiceData: [],
-  eventServiceData: [],
-  multiServicesData: [],
-  propertyServiceData:[],
-  petIDServicesData: [],
-  kidsSafetyServicesData: [],
-  
-  // Encrypted services
-  audioServiceData: [],
-  pdfServiceData: [],
-  videoServiceData: [],
-  galleryServiceData: [],
-  error: null,
+    resumeServiceData: [],
+    wifiServicesData: [],
+    menuCardServiceData: [],
+    textMessageData: [],
+    smsServiceData: [],
+    vehicleServiceData: [],
+    eventServiceData: [],
+    multiServicesData: [],
+    propertyServiceData: [],
+    petIDServicesData: [],
+    kidsSafetyServicesData: [],
+    pdfServiceData: [],
+    audioServiceData: [],
+    videoServiceData: [],
+    galleryServiceData: [],
+    error: null,
 };
 
 const servicesSlice = createSlice({
-  name: "services",
-  initialState,
-  reducers: {
-    // ✅ Regular service reducers
-    setResumeServices: (state, action) => {
-      state.resumeServiceData = action.payload;
-      state.error = null;
+    name: "services",
+    initialState,
+    reducers: {
+        // Regular Services
+        setResumeServices: (state, action) => ({
+            ...state,
+            resumeServiceData: action.payload,
+            error: null,
+        }),
+        setMenuCardServices: (state, action) => ({
+            ...state,
+            menuCardServiceData: action.payload,
+            error: null,
+        }),
+        setTextMessageServices: (state, action) => ({
+            ...state,
+            textMessageData: action.payload,
+            error: null,
+        }),
+        setSmsServices: (state, action) => ({
+            ...state,
+            smsServiceData: action.payload,
+            error: null,
+        }),
+        setVehicleServices: (state, action) => ({
+            ...state,
+            vehicleServiceData: action.payload,
+            error: null,
+        }),
+        setEventServices: (state, action) => ({
+            ...state,
+            eventServiceData: action.payload,
+            error: null,
+        }),
+        setMultiUrlServices: (state, action) => ({
+            ...state,
+            multiServicesData: action.payload,
+            error: null,
+        }),
+        setWifiServices: (state, action) => ({
+            ...state,
+            wifiServicesData: action.payload,
+            error: null,
+        }),
+        setPropertyServices: (state, action) => ({
+            ...state,
+            propertyServiceData: action.payload,
+            error: null,
+        }),
 
-      return {
-        ...state,
-        resumeServiceData: action.payload,
-        error: null,
-      };
-    },
+        // Encrypted Services
+        setPetIdServices: (state, action) => ({
+            ...state,
+            petIDServicesData: action.payload,
+            error: null,
+        }),
+        setKidsSafetyServices: (state, action) => ({
+            ...state,
+            kidsSafetyServicesData: action.payload,
+            error: null,
+        }),
 
+        // Error handling
+        clearError: (state) => ({
+            ...state,
+            error: null,
+        }),
     },
-    setMenuCardServices: (state, action) => {
-      state.menuCardServiceData = action.payload;
-      state.error = null;
-    },
-    setTextMessageServices: (state, action) => {
-      state.textMessageData = action.payload;
-      state.error = null;
-    },
-    setSmsServices: (state, action) => {
-      state.smsServiceData = action.payload;
-      state.error = null;
-    },
-    setVehicleServices: (state, action) => {
-      state.vehicleServiceData = action.payload;
-      state.error = null;
-    },
-    setEventServices: (state, action) => {
-      state.eventServiceData = action.payload;
-      state.error = null;
-    },
-    setMultiUrlServices: (state, action) => {
-      state.multiServicesData = action.payload;
-      state.error = null;
-    },
-    setWifiServices: (state, action) => {
-      state.wifiServicesData = action.payload;
-      state.error = null;
-    },
-    setPropertyServices:(state, action) => {
-      state.propertyServiceData = action.payload;
-      state.error=null;
-    },
-
-    // ✅ Encrypted service reducers
-    setPDFServices: (state, action) => {
-      state.pdfServiceData = action.payload;
-      state.error = null;
-    },
-    },
-    setVehicleServices: (state, action) => {
-      state.vehicleServiceData = action.payload;
-      state.error = null;
-    },
-    setEventServices: (state, action) => {
-      state.eventServiceData = action.payload;
-      state.error = null;
-    },
-    setMultiUrlServices: (state, action) => {
-      state.multiServicesData = action.payload;
-      state.error = null;
-    },
-    setWifiServices: (state, action) => {
-      state.wifiServicesData = action.payload;
-      state.error = null;
-    },
-    setPetIdServices: (state, action) => {
-      state.petIDServicesData = action.payload;
-      state.error = null;
-    },
-    setKidsSafetyServices: (state, action) => {
-      state.kidsSafetyServicesData = action.payload;
-      state.error = null;
-    },
-    // ✅ Encrypted service reducers
-    setPDFServices: (state, action) => {
-      state.pdfServiceData = action.payload;
-      state.error = null;
-    },
-
-
-    setEventServices: (state, action) => {
-      return {
-        ...state,
-        eventServiceData: action.payload,
-        error: null,
-      };
-    },
-    setMultiUrlServices: (state, action) => {
-      return {
-        ...state,
-        multiServicesData: action.payload,
-        error: null,
-      };
-    },
-    setWifiServices: (state, action) => {
-      return {
-        ...state,
-        wifiServicesData: action.payload,
-        error: null,
-      };
-
-    setAudioServices: (state, action) => {
-      state.audioServiceData = action.payload;
-      state.error = null;
-    },
-    setVideoServices: (state, action) => {
-      state.videoServiceData = action.payload;
-      state.error = null;
-    },
-    setGalleryServices: (state, action) => {
-      state.galleryServiceData = action.payload;
-      state.error = null;
-    },
-    setVideoServices: (state, action) => {
-      state.videoServiceData = action.payload;
-      state.error = null;
-    },
-    setGalleryServices: (state, action) => {
-      state.galleryServiceData = action.payload;
-      state.error = null;
-    },
-   
-
-    // Clear error globally
-    clearError: (state) => {
-      state.error = null;
-    },
-  },
 });
 
 export const {
-  setResumeServices,
-  setMenuCardServices,
-  setTextMessageServices,
-  setSmsServices,
-  setVehicleServices,
-  setEventServices,
-  setMultiUrlServices,
-  setWifiServices,
-  setPropertyServices,
+    setResumeServices,
+    setMenuCardServices,
+    setTextMessageServices,
+    setSmsServices,
+    setVehicleServices,
+    setEventServices,
+    setMultiUrlServices,
+    setWifiServices,
+    setPropertyServices,
+    setPetIdServices,
+    setKidsSafetyServices,
 
-  setPDFServices,
-  setAudioServices,
-  setVideoServices,
-  setGalleryServices,
-
-  setEventServices,
-  setTextMessageServices,
-  setSmsServices,
-  setMultiUrlServices,
-  setVehicleServices,
-  setWifiServices,
-
-  setTextMessageServices,
-  setSmsServices,
-  setVehicleServices,
-  setEventServices,
-  setMultiUrlServices,
-  setWifiServices,
-  setPDFServices,
-  setAudioServices,
-  setVideoServices,
-  setGalleryServices,
-  clearError,
+    clearError,
 } = servicesSlice.actions;
 
 export default servicesSlice.reducer;
