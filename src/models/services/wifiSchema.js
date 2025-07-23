3bffae94bf2327d0caeeb69007567aaf1e273f92
pacchava mounika <pacchavamounika@gmail.com>
2025-07-23 10:59:28 +0530

    add changes in services,qrshapes,stickers

--- src/models/services/wifiSchema.js
+++ src/models/services/wifiSchema.js
@@ -32,30 +32,71 @@
 
 
 
+// import mongoose from "mongoose";
+
+// const wifiSchema = new mongoose.Schema(
+//   {
+//     ssid: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     security: {
+//       type: String,
+//       enum: ["WPA", "WEP", "nopass"],
+//       required: true,
+//     },
+//     password: {
+//       type: String,
+//       required: function () {
+//         return this.security !== "nopass";
+//       },
+//     },
+//     qrPassword : {
+//       type: String, // This field is optional and can be used to store a QR code representation of the password
+//       trim: true,
+//     }
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const WifiModel = mongoose.models.Wifi || mongoose.model("Wifi", wifiSchema);
+
+// export default WifiModel;
+
 import mongoose from "mongoose";
+import { qrCodeServicesSchema } from "./qrCodeServicesSchema";
+
 
 const wifiSchema = new mongoose.Schema(
   {
+    user: {
+      id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+      name: { type: String, required: true },
+    },
+
     ssid: {
       type: String,
       required: true,
       trim: true,
     },
+
     security: {
       type: String,
       enum: ["WPA", "WEP", "nopass"],
       required: true,
     },
+
     password: {
       type: String,
       required: function () {
         return this.security !== "nopass";
       },
     },
-    qrPassword : {
-      type: String, // This field is optional and can be used to store a QR code representation of the password
-      trim: true,
-    }
+
+   qrCodeDetails: qrCodeServicesSchema
   },
   {
     timestamps: true,
