8f8bf7fc620d83b9598ac86661d6fb0d52958781
bunny <akrishtej@gmail.com>
2025-07-30 10:50:25 +0530

    added scancount,location,lastscan

--- src/models/shortLinkSchema.js
+++ src/models/shortLinkSchema.js
@@ -1,9 +1,11 @@
-// models/shortLinkSchema.js
 import mongoose from "mongoose";
 
 const shortLinkSchema = new mongoose.Schema({
   code: { type: String, required: true, unique: true },
   fullUrl: { type: String, required: true },
+  scanCount: { type: Number, default: 0 },
+  lastScanLocation: { type: String },
+  lastScannedAt: { type: Date }, // ✅ Make sure this field name is correct
 });
 
 export default mongoose.models.ShortLink || mongoose.model("ShortLink", shortLinkSchema);
